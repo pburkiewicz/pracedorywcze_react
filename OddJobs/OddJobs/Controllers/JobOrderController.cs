a8298cffc16038284c75a0113977f4c740eda578
GrzegorzDomaradzki <grzedom@onet.pl>
2021-04-26 17:45:19 +0200

    Fetch controller.

--- OddJobs/OddJobs/Controllers/JobOrderController.cs
+++ OddJobs/OddJobs/Controllers/JobOrderController.cs
@@ -1,7 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OddJobs.Data;
+using OddJobs.Models;
+
 namespace OddJobs.Controllers
 {
+    [Route("[controller]")]
     public class JobOrderController
     {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<JobOrderController> _logger;
+
+        public JobOrderController(ApplicationDbContext context, ILogger<JobOrderController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet("{l}/{r}/{u}/{d}")]
+        public IEnumerable<JobOrder> FetchData(double left,double right, double up, double down)
+        {
+            return from jobOrder in _context.JobOrders
+                where jobOrder.Latitude>down && jobOrder.Latitude<up &&
+                      jobOrder.Longitude>left && jobOrder.Longitude>right 
+                        select jobOrder;
+        }
         
     }
 }
