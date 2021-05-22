bf37545987551e08114181d7dfe3b67a0031979c
pburkiewicz <burkiewicz.patrycja@gmail.com>
2021-05-22 13:15:57 +0200

    Change DetailMap from class to hook

--- OddJobs/OddJobs/ClientApp/src/components/DetailsMap.js
+++ OddJobs/OddJobs/ClientApp/src/components/DetailsMap.js
@@ -1,53 +1,30 @@
-﻿import React from "react";
+﻿import React, {useEffect, useState} from "react";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import {greenIcon} from "./Icons"
 import 'leaflet.locatecontrol';
 import 'leaflet-geosearch/dist/geosearch.css';
 
-class DetailsMap extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            position: [51.981497, 20.143433],
-            zoom: 7,
-            bounds: null
+const DetailsMap = (props) => {
+    const [position, setPosition] = useState([props.latitude, props.longitude])
+    const [zoom, setZoom] = useState(13);
+    let mapBox = null
+    let markerLayer = null 
+        
+    useEffect( () =>{
+        map();
+    }, []);
 
-        };
-        this.props = props;
-        this.OnUpdateMarkers = this.OnUpdateMarkers.bind(this);
-    }
-
-    componentDidMount() {
-            this.map();
-    }
-
-    map() {
-        this.mapBox = L.map('map').setView([this.props.latitude, this.props.longitude], 13);
-        this.mapBox.addLayer(L.marker([this.props.latitude, this.props.longitude], {icon: greenIcon}));
-        // this.mapBox.locate({setView : true});
-        L.control.locate().addTo(this.mapBox);
-        this.mapBox.setView([this.props.latitude, this.props.longitude], 13);
-        this.markerLayer = L.layerGroup().addTo(this.mapBox);
+    const map = () => {
+        mapBox = L.map('map').setView(position, zoom);
+        mapBox.addLayer(L.marker(position, {icon: greenIcon}));
+        L.control.locate().addTo(mapBox);
+        markerLayer = L.layerGroup().addTo(mapBox);
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(this.mapBox);
-        this.mapBox.on('moveend',this.OnUpdateMarkers);
+        }).addTo(mapBox);
     }
-
-    async OnUpdateMarkers() {
-        const bounds = this.mapBox.getBounds();
-        const zoom = this.mapBox.getZoom()
-        const position = this.mapBox.getCenter();
-        this.setState({
-            bounds: bounds,
-            zoom: zoom,
-            position: position
-        });
-    }
-
-    render() {
-        return <div id="map" style={{width:'100%', height: '100%'}}>Generowanie mapy</div>
-    }
+    
+    return <div id="map" style={{width:'100%', height: '100%'}}>Generowanie mapy</div>
 }
 export default DetailsMap;
