6933557bf4cbb2a8d68962546311c9e236fc26e3
pburkiewicz <burkiewicz.patrycja@gmail.com>
2021-06-10 13:05:54 +0200

    Style changes in menu: add hover, change font size, add main icon color. Disable scroll on render.

--- OddJobs/OddJobs/ClientApp/src/components/NavMenu.js
+++ OddJobs/OddJobs/ClientApp/src/components/NavMenu.js
@@ -1,17 +1,8 @@
 ﻿import React, { useState } from 'react';
 import {
-    Collapse,
     Navbar,
-    NavbarToggler,
     NavbarBrand,
     Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText
 } from 'reactstrap';
 import {faBriefcase} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -20,12 +11,10 @@ import {LoginMenu} from "./api-authorization/LoginMenu";
 const NavMenu = (props) => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
-
     return (
             <Navbar color="dark" dark expand={"xs"}  className={"w-100 p-0"} fixed="top">
                 <div className={"m-0 p-2 pl-0 d-flex align-items-center"} style={{background: "#1f1e1e", width: "75px", height: "75px"}}>
-                    <FontAwesomeIcon icon={faBriefcase} className="mx-auto" size="3x" style={{color: "#d5d5d5"}}  />
+                    <FontAwesomeIcon icon={faBriefcase} className="mx-auto" size="3x" style={{color: "#4aba70"}}  />
                 </div>
                 <NavbarBrand href="/"  className={"col-1 pl-2"} style={{color: "#d5d5d5"}}>Prace Dorywcze</NavbarBrand>
                 {/*<NavbarToggler onClick={toggle} />*/}
