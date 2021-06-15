1cfbadd691250dfd8ad7a8da15d6fb45be997291
GrzegorzDomaradzki <grzedom@onet.pl>
2021-06-15 22:14:21 +0200

    Documentation

--- OddJobs/OddJobs/Controllers/JobOrderController.cs
+++ OddJobs/OddJobs/Controllers/JobOrderController.cs
@@ -21,30 +21,40 @@ namespace OddJobs.Controllers
         private readonly ILogger<JobOrderController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public JobOrderController(ApplicationDbContext context, ILogger<JobOrderController> logger, UserManager<ApplicationUser> userManager)
+        public JobOrderController(ApplicationDbContext context, ILogger<JobOrderController> logger,
+            UserManager<ApplicationUser> userManager)
         {
             _context = context;
             _logger = logger;
             _userManager = userManager;
         }
 
-        //https://localhost:5001/JobOrder/FetchData/l=18.997853&r=18.973813&u=50.199828&d=50.193317
         [HttpGet("fetchdata/{left:double}/{right:double}/{up:double}/{down:double}")]
-        public IEnumerable<JobOrder> FetchData(double left,double right, double up, double down)
+        /**
+        * fetch list of all job orders from the area with the given boundaries
+        **/
+        public IEnumerable<JobOrder> FetchData(double left, double right, double up, double down)
         {
             return (from jobOrder in _context.JobOrders
-                where jobOrder.Active && jobOrder.Latitude>down && jobOrder.Latitude<up &&
-                      jobOrder.Longitude>left && jobOrder.Longitude<right 
-                        select jobOrder).ToList();
+                where jobOrder.Active && jobOrder.Latitude > down && jobOrder.Latitude < up &&
+                      jobOrder.Longitude > left && jobOrder.Longitude < right
+                select jobOrder).ToList();
         }
 
         [HttpGet("fetchdata/{lat}/{lng}/{buff}")]
+        /**
+        * fetch list of all job orders from circle area where coordinates of center are in lat and lng (latitude, longitude)
+        *  and radius is provided in buff parameter
+        **/
         public IEnumerable<Tuple<JobOrder, double>> FetchDataBuffer(double lat, double lng, double buff)
         {
-            return GetByDistance(lat,lng,buff);
+            return GetByDistance(lat, lng, buff);
         }
 
         [HttpGet("fetchdata/{principalName}")]
+        /**
+        * fetch list of all job orders witch principal having given name.
+        **/
         public IEnumerable<JobOrder> FetchDataUser(string principalName)
         {
             var id = (from user in _context.Users where user.UserName.Equals(principalName) select user.Id).First();
@@ -52,24 +62,35 @@ namespace OddJobs.Controllers
                 where jobOrder.PrincipalId.Equals(id)
                 select jobOrder).ToList();
         }
-        
+
         [HttpGet("fetchReported/{lat}/{lng}")]
-        [Authorize(Roles="Moderator")]
-        public   IEnumerable<Tuple<JobOrder, double>> FetchReported(double lat, double lng)
+        [Authorize(Roles = "Moderator")]
+        /**
+        //moderator only
+        * fetch list of all reported job orders from circle area where coordinates of center are in lat and lng (latitude, longitude)
+        *  and radius is provided in buff parameter
+        **/
+        public IEnumerable<Tuple<JobOrder, double>> FetchReported(double lat, double lng)
         {
-        return GetByDistance(lat, lng, 500, true);
+            return GetByDistance(lat, lng, 500, true);
         }
-        
+
         [HttpGet("fetchHighStatus")]
         [Authorize]
+        /**
+        * logged only - control if user is in role moderator - return 1 or 0 (true, false)
+        **/
         public int FetchStatus()
         {
             var ret = User.IsInRole("Moderator") ? 1 : 0;
             return ret;
         }
-        
+
         [HttpPut("ReportReaction/{id:int}/{status:int}")]
-        [Authorize(Roles="Moderator")]
+        [Authorize(Roles = "Moderator")]
+        /**
+        * moderator only - react to job report - change reported state of work with given id to chosen status (1 - authorise offer, 0 - ban).
+        **/
         public async Task<IActionResult> ReportReaction(int id, int status)
         {
             var job = await _context.JobOrders.FindAsync(id);
@@ -80,21 +101,28 @@ namespace OddJobs.Controllers
                 job.Reported = -1;
                 job.Active = false;
             }
+
             await _context.SaveChangesAsync();
             return Ok();
         }
 
         [HttpGet("api/{id:int}")]
+        /**
+        * get JobOrder object with the given id.
+        **/
         public async Task<IActionResult> GetJob(int id)
         {
             var job = await _context.JobOrders.Where(j => j.ID == id).Include(j => j.Worker)
-                .Include(j=> j.Principal).ToListAsync();
+                .Include(j => j.Principal).ToListAsync();
             if (job != null) return Ok(job.First());
             return NotFound();
         }
-        
+
         [HttpPut("api/{id:int}")]
         [Authorize]
+        /**
+        * logged only, owner only - edit job with given id based on provided jobForm
+        **/
         public async Task<IActionResult> UpdateJob(int id, [FromBody] JobForm jobForm)
         {
             if (jobForm.Description.Length > 2000 || jobForm.Title.Length > 200) return BadRequest();
@@ -117,6 +145,9 @@ namespace OddJobs.Controllers
 
         [HttpPut("report/{id:int}")]
         [Authorize]
+        /**
+        * logged only - report job with given id to moderation
+        **/
         public async Task<IActionResult> ReportJob(int id, [FromBody] string userId)
         {
             var job = await _context.JobOrders.FindAsync(id);
@@ -125,44 +156,51 @@ namespace OddJobs.Controllers
             await _context.SaveChangesAsync();
             return Ok();
         }
-        
+
         [HttpPut("status/{id:int}")]
         [Authorize]
+        /**
+        * logged only, owner only - change job status from active to inactive  and vice versa
+        **/
         public async Task<IActionResult> ChangeStatus(int id)
         {
             var user = await _userManager.GetUserAsync(User);
             var job = await _context.JobOrders.FindAsync(id);
-            
+
             if (job == null) return NotFound();
-            if(user.Id != job.PrincipalId) return Unauthorized();
+            if (user.Id != job.PrincipalId) return Unauthorized();
             if (job.Reported == -1) return Unauthorized();
-            
+
             job.Active = !job.Active;
 
-            if (job.Active && job.WorkerId != null) {
+            if (job.Active && job.WorkerId != null)
+            {
                 job.Worker = null;
                 job.WorkerId = null;
             }
-            
+
             await _context.SaveChangesAsync();
             return Ok();
         }
-        
+
         [HttpPut("api/{id:int}/assignWorker")]
         [Authorize]
+        /**
+        * logged only, owner only - assign chosen worker to job with given id.
+        **/
         public async Task<IActionResult> AssignWorker(int id, [FromBody] string workerId)
         {
             var user = await _userManager.GetUserAsync(User);
             var job = await _context.JobOrders.FindAsync(id);
-            
+
             if (job == null) return NotFound();
-            if(user.Id != job.PrincipalId) return Unauthorized();
+            if (user.Id != job.PrincipalId) return Unauthorized();
 
             var worker = await _context.ApplicationUsers.FindAsync(workerId);
 
             var thread = await _context.Threads.Include(t => t.JobOrder)
                 .Where(t => t.JobOrder.ID == job.ID && t.InterestedUser.Id == workerId).FirstOrDefaultAsync();
-            
+
             job.Active = false;
             job.Worker = worker;
             job.WorkerId = workerId;
@@ -176,13 +214,16 @@ namespace OddJobs.Controllers
                 Thread = thread,
                 SpecialMessage = true
             });
-            
+
             await _context.SaveChangesAsync();
             return Ok();
         }
 
         [HttpDelete("api/{id:int}")]
         [Authorize]
+        /**
+        * logged only, owner only - delete job with given id.
+        **/
         public async Task<IActionResult> DeleteJob(int id)
         {
             var job = await _context.JobOrders.FindAsync(id);
@@ -193,15 +234,18 @@ namespace OddJobs.Controllers
             await _context.SaveChangesAsync();
             return Ok();
         }
-        
+
         [HttpPost("add")]
         [Authorize]
+        /**
+        * logged only - create new jobOrder based on provided form
+        **/
         public async Task<IActionResult> AddJob([FromBody] JobForm jobForm)
         {
-            // var user = HttpContext.User.Identity.Name;
+// var user = HttpContext.User.Identity.Name;
             if (jobForm.Description.Length > 2000 || jobForm.Title.Length > 200) return BadRequest();
             var user = await _userManager.FindByIdAsync(jobForm.User);
-            
+
             var jobOrder = new JobOrder
             {
                 Title = jobForm.Title,
@@ -220,13 +264,17 @@ namespace OddJobs.Controllers
             };
             _context.JobOrders.Add(jobOrder);
             await _context.SaveChangesAsync();
-            
+
             return Ok(jobOrder);
         }
 
-        public IEnumerable<Tuple<JobOrder, double>> GetByDistance(double lat, double lng, double buff, bool report = false)
+        /**
+* method for distance calculation - circle coordinates, radius, and report modifier - returns jobs in calculated area.
+**/
+        public IEnumerable<Tuple<JobOrder, double>> GetByDistance(double lat, double lng, double buff,
+            bool report = false)
         {
-            return  _context.JobOrders.Select(jobOrder => new
+            return _context.JobOrders.Select(jobOrder => new
                 {
                     jobOrder,
                     dist = 6371 * Math.Acos(
@@ -234,44 +282,52 @@ namespace OddJobs.Controllers
                         Math.Cos(Math.PI / 180 * jobOrder.Longitude - Math.PI / 180 * lng) +
                         Math.Sin(Math.PI / 180 * lat) * Math.Sin(Math.PI / 180 * jobOrder.Latitude))
                 })
-                .Where(@t => @t.jobOrder.Active && @t.dist < buff && (!report || t.jobOrder.Reported==1))
+                .Where(@t => @t.jobOrder.Active && @t.dist < buff && (!report || t.jobOrder.Reported == 1))
                 .OrderBy(@t => @t.dist).Take(100)
-                .Select(@t =>   new Tuple<JobOrder,double>(@t.jobOrder, @t.dist)).ToList();
+                .Select(@t => new Tuple<JobOrder, double>(@t.jobOrder, @t.dist)).ToList();
         }
 
-        
+
         [HttpPost("api/{jobId:int}/send")]
         [Authorize]
+        /**
+        * send first message associated with job having given id. In the same time produce new thread and assign users.
+        **/
         public async Task<IActionResult> SendFirstMessage(int jobId, [FromBody] BasicMessage message)
         {
             if (message.MessageText.Length == 0 || message.MessageText.Length > 200) return BadRequest();
             var jobOrder = await _context.JobOrders.FindAsync(jobId);
             var user = await _userManager.FindByIdAsync(message.User);
-            
-            var thread = new Thread {
+
+            var thread = new Thread
+            {
                 JobOrder = jobOrder,
                 InterestedUser = user,
                 InterestedUserRead = true,
                 PrincipalRead = false
             };
-            
-            var mes = new Message {
+
+            var mes = new Message
+            {
                 MessageText = message.MessageText,
                 Thread = thread,
                 SendTime = DateTime.Now,
                 Sender = user,
                 SpecialMessage = false
             };
-            
+
             _context.Threads.Add(thread);
             _context.Messages.Add(mes);
-            
+
             await _context.SaveChangesAsync();
-            
+
             return Ok(thread);
         }
     }
-    
+
+    /**
+* class for providing job info
+**/
     public class JobForm
     {
         public string Title { get; set; }
@@ -283,6 +339,7 @@ namespace OddJobs.Controllers
         public string Address { get; set; }
         public string User { get; set; }
     }
+
     public class BasicMessage
     {
         public string MessageText { get; set; }
--- OddJobs/OddJobs/Controllers/MessageController.cs
+++ OddJobs/OddJobs/Controllers/MessageController.cs
@@ -22,63 +22,76 @@ namespace OddJobs.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
 
         public MessageController(ApplicationDbContext context, ILogger<MessageController> logger,
-            UserManager<ApplicationUser> userManager) => (_context, _logger, _userManager) = (context, logger, userManager);
+            UserManager<ApplicationUser> userManager) =>
+            (_context, _logger, _userManager) = (context, logger, userManager);
 
 
         [HttpGet("api/{threadId:Guid}/messages")]
         [Authorize]
+        /**
+        * Get messages from given threadId sorted in chronological order
+        * User must be logged and added to conversation
+        **/
         public async Task<IActionResult> GetMessages(Guid threadId)
         {
             var threads = await _context.Threads.Where(t => t.Id == threadId)
-                .Include(t => t.JobOrder).Include(t=>t.InterestedUser).ToListAsync();
-            
+                .Include(t => t.JobOrder).Include(t => t.InterestedUser).ToListAsync();
+
             if (threads.Count == 0) return NotFound();
-            
+
             var thread = threads.First();
- 
+
             var user = await _userManager.GetUserAsync(User);
-            
+
             if (!(user.Id != thread.InterestedUser.Id || user.Id != thread.JobOrder.PrincipalId)) return Unauthorized();
 
             var query = await _context.Messages.Where(m => m.Thread.Id == threadId)
-                .Include(m=>m.Sender).OrderByDescending(m=> m.SendTime).ToListAsync();
-            
+                .Include(m => m.Sender).OrderByDescending(m => m.SendTime).ToListAsync();
+
             return Ok(query);
         }
-        
+
         [HttpGet("api/{threadId:Guid}")]
         [Authorize]
+        /**
+        * Get threat with the given threadId
+        * User must be logged and added to conversation
+                **/
         public async Task<IActionResult> GetThread(Guid threadId)
         {
             var user = await _userManager.GetUserAsync(User);
-            
-            var query = await _context.Threads.Where(t => t.Id == threadId).Include(t=> t.InterestedUser)
-                .Include(t => t.JobOrder).ThenInclude(t=> t.Principal).ToListAsync();
+
+            var query = await _context.Threads.Where(t => t.Id == threadId).Include(t => t.InterestedUser)
+                .Include(t => t.JobOrder).ThenInclude(t => t.Principal).ToListAsync();
 
             if (query.Count == 0) return NotFound();
-            
+
             var thread = query.First();
 
             if (!(user.Id != thread.InterestedUser.Id || user.Id != thread.JobOrder.PrincipalId)) return Unauthorized();
-            
+
             return Ok(thread);
         }
-        
-        
+
+
         [HttpPost("api/{threadId:Guid}")]
         [Authorize]
+        /**
+        * Add given message (from body) to thread with given id
+        * User must be logged and added to conversation
+            **/
         public async Task<IActionResult> SendMessage(Guid threadId, [FromBody] BasicMessage message)
         {
             if (message.MessageText.Length == 0 || message.MessageText.Length > 200) return BadRequest();
             var threads = await _context.Threads.Where(t => t.Id == threadId)
-                .Include(t => t.JobOrder).Include(t=>t.InterestedUser).ToListAsync();
-            
+                .Include(t => t.JobOrder).Include(t => t.InterestedUser).ToListAsync();
+
             if (threads.Count == 0) return NotFound();
-            
+
             var thread = threads.First();
- 
+
             var user = await _userManager.GetUserAsync(User);
-            
+
             if (!(user.Id != thread.InterestedUser.Id || user.Id != thread.JobOrder.PrincipalId)) return Unauthorized();
 
             if (user.Id == thread.InterestedUser.Id)
@@ -92,43 +105,59 @@ namespace OddJobs.Controllers
                 thread.PrincipalRead = true;
             }
 
-            var mes = new Message {
+            var mes = new Message
+            {
                 MessageText = message.MessageText,
                 Thread = thread,
                 SendTime = DateTime.Now,
                 Sender = user,
                 SpecialMessage = false
             };
-                        
+
             _context.Messages.Add(mes);
-            
+
             await _context.SaveChangesAsync();
-            
+
             return Ok();
         }
 
         [HttpGet("api/getThreads")]
         [Authorize]
+        /**
+        * Get list of threads to whose user is added
+        * User must be logged
+                **/
         public async Task<IActionResult> GetThreads()
         {
             var user = await _userManager.GetUserAsync(User);
-           
+
             var messages = _context.Messages.Where(message => message.Thread.JobOrder.PrincipalId == user.Id
-                                                                    || message.Thread.InterestedUser.Id == user.Id)
-                .Include("Thread").Include("Thread.JobOrder").Include("Thread.JobOrder.Principal").Include("Thread.InterestedUser")
+                                                              || message.Thread.InterestedUser.Id == user.Id)
+                .Include("Thread").Include("Thread.JobOrder").Include("Thread.JobOrder.Principal")
+                .Include("Thread.InterestedUser")
                 .AsEnumerable()
                 .GroupBy(x => x.Thread.Id).Select(x => x.Last())
-                .Select(m => new {message=m, correspondent= user.Id == m.Thread.InterestedUser.Id ? m.Thread.JobOrder.Principal : m.Thread.InterestedUser});
-       
-           return user != null ? Ok(messages) : Ok(false);
+                .Select(m => new
+                {
+                    message = m,
+                    correspondent = user.Id == m.Thread.InterestedUser.Id
+                        ? m.Thread.JobOrder.Principal
+                        : m.Thread.InterestedUser
+                });
+
+            return user != null ? Ok(messages) : Ok(false);
         }
 
         [HttpPost("api/{jobId:int}/getThread")]
         [Authorize]
+        /**
+        * Get thread for job witch given jobId. Thread is returned based on user.
+        * User must be logged
+                **/
         public async Task<IActionResult> GetThreadFromJobId(int jobId)
         {
             var user = await _userManager.GetUserAsync(User);
-            
+
             var thread = await _context.Threads.Where(t => t.JobOrder.ID == jobId && t.InterestedUser.Id == user.Id)
                 .ToListAsync();
 
@@ -137,14 +166,18 @@ namespace OddJobs.Controllers
 
         [HttpGet("api/{jobId:int}/getInterestedUsers")]
         [Authorize]
+        /**
+        * Get list of users who subscribed (send message) to job offer with the given id
+        * User must be logged
+                **/
         public async Task<IActionResult> GetInterestedUsers(int jobId)
         {
             var user = await _userManager.GetUserAsync(User);
             var job = await _context.JobOrders.FindAsync(jobId);
 
             if (job == null) return NotFound();
-            if(job.PrincipalId != user.Id) return Unauthorized();
-            
+            if (job.PrincipalId != user.Id) return Unauthorized();
+
             var interestedUsers = await _context.Threads.Where(t => t.JobOrder.ID == jobId)
                 .Select(t => t.InterestedUser).ToListAsync();
 
@@ -153,11 +186,15 @@ namespace OddJobs.Controllers
 
         [HttpPut("api/{threadId:guid}")]
         [Authorize]
+        /**
+        * Mark thread with the given number as seen
+        * User must be logged
+                **/
         public async Task<IActionResult> ReadMessage(Guid threadId)
         {
             var user = await _userManager.GetUserAsync(User);
             var thread = await _context.Threads.Include(t => t.JobOrder)
-                .Include(t=> t.InterestedUser).FirstOrDefaultAsync(t=> t.Id == threadId);
+                .Include(t => t.InterestedUser).FirstOrDefaultAsync(t => t.Id == threadId);
 
             if (thread == null) return NotFound();
             if (!(user.Id != thread.InterestedUser.Id || user.Id != thread.JobOrder.PrincipalId)) return Unauthorized();
@@ -172,12 +209,16 @@ namespace OddJobs.Controllers
             }
 
             await _context.SaveChangesAsync();
-            
+
             return Ok();
         }
 
         [HttpGet("api/unread")]
         [Authorize]
+        /**
+        * Remove seen flag from thread with given id.
+        * User must be logged
+                **/
         public async Task<IActionResult> UnreadMessages()
         {
             var user = await _userManager.GetUserAsync(User);
